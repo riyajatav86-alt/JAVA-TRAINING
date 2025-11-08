557e689194a42431eab4987e278fb44f0d004935
riya jatav <riyajatav86@.com>
2025-11-08 18:11:05 +0530

    java

---
+++ stringcopy.java
@@ -0,0 +1,11 @@
+
+public class stringcopy {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String copy = new String(original);
+        System.out.println("Original String: " + original);
+        System.out.println("Copied String: " + copy);
+    }
+    
+    
+}
