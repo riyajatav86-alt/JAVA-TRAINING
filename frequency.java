23330237af139ad66c317aef73d7805ab6ddb9b0
riya jatav <riyajatav86@.com>
2025-11-08 17:30:57 +0530

    code

---
+++ frequency.java
@@ -0,0 +1,24 @@
+public class frequency {
+    public static void main(String[] args) {
+        
+        String str = "hello world";
+        int[] freq = new int[256]; // ASCII character set size
+
+        // Calculate frequency of each character
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        // Print frequency of each character
+        System.out.println("Character Frequency:");
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char)i + ": " + freq[i]);
+            }
+        }
+    }
+    
+
+
+    
+}
