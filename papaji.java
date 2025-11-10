120cecebd70e7aee34117bb0c2d62633d8b529a6
riya jatav <riyajatav86@.com>
2025-11-10 23:14:59 +0530

    riya

---
+++ papaji.java
@@ -0,0 +1,9 @@
+public class papaji {
+    public static void main(String[] args) {
+        String str = "papaji is great";
+       
+
+        System.out.println("Original String: " + str);
+    }
+    
+}
