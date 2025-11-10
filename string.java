f2523b43a506f28f51855b038d1c0b917be9c8a6
riya jatav <riyajatav86@.com>
2025-11-10 12:41:01 +0530

    java

---
+++ string.java
@@ -0,0 +1,18 @@
+class main {
+    public static void main(String[] args) {
+       // String s1="hello";
+        //string s2="";
+       // for (char ch : s1.toCharArray()) {
+          //  s2+=ch;
+            
+        }
+       // stringBuilder sb=new stringBuilder(str:"hello");
+        
+       // System.out.println(str.length());
+       // str.append("");
+       // system.out.println(str);
+        //system.out.println();
+        //str.insert(offset:2,str:" byee");
+       // System.out.println(str);
+    }
+}
