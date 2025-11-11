537e29092c83c883fba674461f4f698b0f060299
riya jatav <riyajatav86@.com>
2025-11-11 12:52:24 +0530

    java

---
+++ method.java
@@ -0,0 +1,10 @@
+public class method {
+    void sum(int a){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        method obj=new method();
+        obj.sum(5);
+    }
+    
+}
--- variable.java
+++ variable.java
@@ -1,12 +1,12 @@
 
 public class variable {
     
-    static int a=10;
-    public static void main(String[] args) {
-        variable obj=new variable();
-        System.out.println(obj.a);
-    }
-}
+  //  static int a=10;
+ //   public static void main(String[] args) {
+   //     variable obj=new variable();
+     //   System.out.println(obj.a);
+    //}
+//}
 
         
        
