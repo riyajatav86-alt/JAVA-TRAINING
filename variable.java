ab0216987ebdc550c8f75836c053e03e9c507f99
riya jatav <riyajatav86@.com>
2025-11-11 12:38:51 +0530

    java

---
+++ variable.java
@@ -0,0 +1,16 @@
+
+public class variable {
+    
+    static int a=10;
+    public static void main(String[] args) {
+        variable obj=new variable();
+        System.out.println(obj.a);
+    }
+}
+
+        
+       
+
+
+
+
