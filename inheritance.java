932341c2030bfe1c144353fcbceb12c56a9e6d86
riya jatav <riyajatav86@.com>
2025-11-19 13:34:56 +0530

    code

---
+++ inheritance.java
@@ -0,0 +1,28 @@
+     public class inheritance {
+     class Animal{
+     void eat(){
+       System.out.println("Pizza");
+        }
+        //void sleep(){
+      // }
+
+      class Dog extends Animal{
+       void eat(){
+        System.out.println("biryani");
+      }
+        void data(){
+        System.out.println("hello");
+        }
+      }    
+      class main{
+        public static void main(String)[] args) {
+       //Dog s1=new Dog();
+        //s1.eat();
+       Animal s2=new Dog();
+        s1.eat();
+       // Dog s2=(Dog)s1;
+       // // s2.
+      }
+      }
+    
+      
