9b6760c6d3207143522d6598aaf6621b189476d7
riya jatav <riyajatav86@.com>
2025-11-26 16:09:11 +0530

    java

---
+++ lec15/src/twosum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class twosum {
+    public static void main(String[]args){
+        int arr[]={2,7,5,4};
+            HashMap<Integer,Integer>map=new HashMap();
+        int target=9;
+        for(int i=0;i<arr.length;i++){
+            int diff=target-arr[i];
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+""+i);
+                break;
+
+            }
+            map.put(arr[i],i);
+        }
+    }
+
+}
