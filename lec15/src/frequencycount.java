bd5b66c59d68ea8b6f337c2c13a94550ebad70d1
riya jatav <riyajatav86@.com>
2025-11-26 15:33:06 +0530

    java

---
+++ lec15/src/frequencycount.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class frequencycount {
+public static void main(String[]args){
+    HashMap<Integer,Integer> map =new HashMap<>();
+    int ar[]={2,2,3,4,2,2,4,2};
+    for(int x :ar){
+        map.put(x, map.getOrDefault(x,0)+1);
+
+    }
+    System.out.println(map);
+}
+}
