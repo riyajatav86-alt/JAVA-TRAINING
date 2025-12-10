74ca878d9cb22c97797d6528933cf9c03bbecf9f
riya jatav <YOUR_CORRECT_EMAIL_HERE>
2025-12-10 15:36:59 +0530

    code

---
+++ lec20/src/file_creation.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+
+public class file_creation {
+
+    public static void main(String[] args) {
+        try {
+
+            File f = new File("home.html");
+
+            if (f.createNewFile()) {
+                System.out.println("File created: " + f.getName());
+            } else {
+                System.out.println("--- sorry!! file of this name is already created ---");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+
