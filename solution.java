63d6d81016073c7e2ebf9a992b712411ad0a23ce
riya jatav <YOUR_CORRECT_EMAIL_HERE>
2025-12-22 16:05:23 +0530

    code

---
+++ solution.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class solution {
+    public int majorityElelment(int[]nums){
+        int count=0;
+
+    int ans=0;
+    for(int i:nums){
+    if(i==0)}
+    }
