150f63d1f9940e09a4596eb829c0ee207b07ddf0
Riya <YOUR_CORRECT_EMAIL_HERE>
2026-01-17 23:03:41 +0530

    your

---
+++ .vscode/inheritance.java/arrayg.java
@@ -0,0 +1,11 @@
+public class arrayg {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Accessing array elements
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+    
+}
