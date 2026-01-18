84e38ea2053f016038fed1ce95d0609bbfb7f7e5
Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>
2026-01-18 22:00:44 +0530

    Add files via upload

---
+++ frequency.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    int arr[]={122,45,78,69,25};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int freq[size];
+    for(int i=0;i<size;i++){
+        freq[i]=-1;
+    }
+    for(int i=0;i<size;i++){
+        int count=1;
+        for(int j=i+1;j<size;j++){
+            if(arr[i]==arr[j]){
+                count++;
+                freq[j]=0;
+            }
+        }
+        if(freq[i]!=0){
+            freq[i]=count;
+        }
+    }
+    printf("Element\tFrequency\n");
+    for(int i=0;i<size;i++){
+        if(freq[i]!=0){
+            printf("%d\t%d\n",arr[i],freq[i]);
+        }
+    }
+    return 0;
+    
+
+}
